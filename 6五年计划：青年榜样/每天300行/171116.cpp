82d7ae03fef6162723db6ecceca30134987aaf43
HanYouyang <hanyouyang93@gmail.com>
2017-11-17 10:27:15 +0800

    转行文章放置

--- 6五年计划：青年榜样/每天300行/171116.cpp
+++ 6五年计划：青年榜样/每天300行/171116.cpp
@@ -153,6 +153,26 @@ int main(){
         stu[num].location_number = i;
         num++;
     }
+    sort(stu + num -k, stu + num, cmp);
+    stu[num - k].local_rank = 1;
+    for(int j = num - k +1; j < num; j++){
+        if(stu[j].score == stu[j-1].score){
+            stu[j].local_rank = stu[j-1].local_rank;
+        }
+        else{
+            stu[j].local_rank = j + 1 - (num - k);
+        } 
+    }
 
-
+printf("%d\n", num);
+sort(stu, stu + num, cmp);
+int r = 1;
+for(int i = 0; i < num; i++){
+    if(i > 0 && stu[i].score != stu[i-1].score){
+        r = i + 1;
+    }
+    printf("%s ", stu[i].id);
+    printf("%d %d %d\n", r, stu[i].location_number, stu[i].local_rank);   
+}
+return 0;
 }
